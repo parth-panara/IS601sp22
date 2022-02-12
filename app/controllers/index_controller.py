64e4bd0d8cb273178c858d248231b65326d60ef6
Parth Panara <parthpanara9@gmail.com>
2022-02-12 01:47:57 -0500

    Update my name again and again

--- app/controllers/index_controller.py
+++ app/controllers/index_controller.py
@@ -6,5 +6,5 @@ from  . controller import ControllerBase
 class IndexController(ControllerBase):
     @staticmethod
     def get():
-        name = "Parth  panara"
+        name = "Parth Panara Hello World"
         return render_template('index.html', name=name)
